3ca03d3037edec1f5ccf13094bc2e138a980c5e0
shou xiang <weeshouxiang@gmail.com>
2025-07-06 13:26:22 +0800

    gnl v2 REAL

--- gnlv2/get_next_line.c
+++ gnlv2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wshou-xi <wshou-xi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 16:30:33 by wshou-xi          #+#    #+#             */
-/*   Updated: 2025/06/30 17:53:46 by wshou-xi         ###   ########.fr       */
+/*   Updated: 2025/07/06 13:26:05 by wshou-xi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = readbuf()
+	buffer = readbuf();
 }
