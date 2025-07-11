dcd33cf0db0d4e3072825b4f1ede4dc2d493c6b6
Wee Shou Xiang <63289858+xiang2007@users.noreply.github.com>
2025-07-11 06:53:51 +0000

    modified code

--- gnlv2/get_next_line.c
+++ gnlv2/get_next_line.c
@@ -14,6 +14,8 @@
 
 char	*readbuf(int fd, char *str, char *temp);
 char	*appendline(char *str, char *remain);
+char	*remainder(char *str);
+int		nloccur(char *str);
 
 char	*get_next_line(int fd)
 {
@@ -25,7 +27,11 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = readbuf(fd, buffer, remain);
-	buffer = appendline(buffer, remain);
+	remain = remainder(buffer);
+	if (!remainder)
+		return (NULL);
+	else if (nloccur(bufer))
+		
 
 }
 
@@ -42,7 +48,7 @@ char	*readbuf(int fd, char *str, char *temp)
 	return (str);
 }
 
-char	*appendline(char *str, char *remain)
+/*char	*appendline(char *str, char *remain)
 {
 	int	i;
 	int	j;
@@ -65,7 +71,46 @@ char	*appendline(char *str, char *remain)
 			i++;
 		}
 		str[j + 2] = '\0';
-		remain[j] = '\0';
+		rema
+}*/
+
+char	*remainder(dchar *str)
+{
+	int		i;
+	int		j;
+	int		size;
+	char	*temp;
+
+	i = 0;
+	size = ft_strlen(str);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+	{
+		temp = (char *)malloc((size - i) + 1);
+		if (!temp)
+			return (NULL);
+		i++;
+		while (str[i])
+			temp[j++] = str[i++];
 	}
-	return (str);
+	if(temp)
+	{
+		temp[j] = '\0';
+		return (temp);
+	}
+	else
+		return (NULL);
 }
+
+int	nloccur(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		return (1);
+	return (0);
+}		
