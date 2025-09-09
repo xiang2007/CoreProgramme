7226b150370aaef968668de0dd2a512f36675992
shou xiang <weeshouxiang@gmail.com>
2025-09-09 14:38:02 +0800

    base rendering finished

--- Fract-ol/fractol.c
+++ Fract-ol/fractol.c
@@ -6,7 +6,7 @@
 /*   By: wshou-xi <wshou-xi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 16:18:25 by wshou-xi          #+#    #+#             */
-/*   Updated: 2025/09/08 16:49:18 by wshou-xi         ###   ########.fr       */
+/*   Updated: 2025/09/09 12:03:28 by wshou-xi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,75 @@ void	ftput_pixel(t_data *img_data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = img_data->addr + (y * img_data->line_length + x * (img_data->bpp / 8));
+	dst = img_data->addr + (int)(y * img_data->line_length + x * (img_data->bpp / 8));
 	*(unsigned int *)dst = color;
 }
-int	get_iter(t_d x, t_d y)
+
+int	get_iter(int x, int y)
 {
-	t_cords	cords;
-	t_d		z;
-	t_d		res;
+	t_calc	calc;
+	t_xy	xy;
 	int		iter;
 
-	cords.res_c = 0;
-	z = 1;
 	iter = 0;
-	while (z < 4 && iter < MAX_ITER)
+	xy.x0 = get_x_scaled(x);
+	xy.y0 = get_y_scaled(y);
+	xy.x = 0;
+	xy.y = 0;
+	xy.x2 = 0;
+	xy.y2 = 0;
+	calc.temp = 0;
+	while ((xy.x2 + xy.y2 <= 4) && iter < MAX_ITER)
 	{
-		cords.res_c = (x * 0.005) + (y * 0.0033);
-		res = d_square(z) + cords.res_c;
-		z = res;
+		xy.y = 2 * xy.x * xy.y + xy.y0;
+		xy.x = xy.x2 - xy.y2 + xy.x0;
+		xy.x2 = d_sq(xy.x);
+		xy.y2 = d_sq(xy.y);
 		iter++;
 	}
 	return (iter);
+}
 
+void	put_mandel(void *img)
+{
+	int	i;
+	int	j;
+	int	iter;
+
+	i = 0;
+	j = 0;
+	while (j <= HEIGTH)
+	{
+		while (i <= 800)
+		{
+			iter = get_iter(i, j);
+			if (iter == MAX_ITER)
+				ftput_pixel(img, i, j, BLACK);
+			else if (iter % 2 == 0)
+				ftput_pixel(img, i, j, BLUE);
+			else if (iter % 3 == 0)
+				ftput_pixel(img, i, j, PURPLE);
+			else
+				ftput_pixel(img, i, j, WHITE);
+			i++;
+		}
+		i = 0;
+		j++;
+	}
 }
 
 int	main(void)
 {
-	//void	*mlx;
-	//void	*mlx_win;
-	//t_data		img;
+	void	*mlx;
+	void	*mlx_win;
+	t_data		img;
 
-	/*mlx = mlx_init();
+	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, WIDTH, HEIGTH, "hello");
 	img.img = mlx_new_image(mlx, WIDTH, HEIGTH);
 	img.addr = mlx_get_data_addr(img.img, &img.bpp,
-								&img.line_length, &img.endian);*/
-	printf("max iter is: %d\n",get_iter(100, 100));
-	//mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	//mlx_loop(mlx);
+								&img.line_length, &img.endian);
+	put_mandel(&img.img);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
--- Fract-ol/includes/fol.h
+++ Fract-ol/includes/fol.h
@@ -6,18 +6,20 @@
 /*   By: wshou-xi <wshou-xi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 16:18:16 by wshou-xi          #+#    #+#             */
-/*   Updated: 2025/09/08 16:59:45 by wshou-xi         ###   ########.fr       */
+/*   Updated: 2025/09/09 12:04:36 by wshou-xi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FOL_H
 # define FOL_H
 
-# define HEIGTH 600
+# define HEIGTH 800
 # define WIDTH 800
-# define WHITE 0xFFFFFFFF
-# define BLACK 0x00FF0000
-# define MAX_ITER 1000
+# define WHITE 0xFFFFFF
+# define BLACK 0x000000
+# define BLUE 0x003366
+# define PURPLE 0x772FB1
+# define MAX_ITER 100
 
 typedef double t_d;
 
@@ -30,14 +32,26 @@ typedef struct	s_data
 	int		endian;
 }				t_data;
 
-typedef struct	s_cords
+typedef struct	s_xy
 {
-	t_d	real_x;
-	t_d	img_y;
-	t_d	res_c;
-	int	iter;
-}				t_cords;
+	t_d	x;
+	t_d	x0;
+	t_d	y;
+	t_d	y0;
+	t_d	x2;
+	t_d	y2;
+}				t_xy;
 
+typedef struct s_calc
+{
+	t_d c;
+	t_d z;
+	t_d temp;
+}				t_calc;
+
+t_d	d_sq(t_d num);
+t_d	get_x_scaled(t_d x);
+t_d	get_y_scaled(t_d y);
 
 # include "../libft/libft.h"
 # include "../mlx_Linux/mlx.h"
