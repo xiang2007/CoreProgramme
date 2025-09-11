727ceb1924009405d1623dfc58ba7463876d0c5a
shou xiang <weeshouxiang@gmail.com>
2025-09-11 17:31:27 +0800

    finished mandelbrot set

--- Fract-ol/utils1.c
+++ Fract-ol/utils1.c
@@ -6,7 +6,7 @@
 /*   By: wshou-xi <wshou-xi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 11:23:27 by wshou-xi          #+#    #+#             */
-/*   Updated: 2025/09/11 16:49:32 by wshou-xi         ###   ########.fr       */
+/*   Updated: 2025/09/11 17:02:29 by wshou-xi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ void	ftput_pixel(t_data *img_data, int x, int y, int color)
 static void control(int key, t_data *win)
 {
 	if (key == XK_a)
-		win->x += 0.1;
+		win->x -= 0.01 / win->zoom;
 	if (key == XK_d)
-		win->x -= 0.1;
+		win->x += 0.01 / win->zoom;
 	if (key == XK_w)
-		win->y += 0.1;
+		win->y -= 0.01 / win->zoom;
 	if (key == XK_s)
-		win->y -= 0.1;
+		win->y += 0.01 / win->zoom;
 	put_mandel(&win->img, win);
 	mlx_put_image_to_window(win->mlx, win->mlx_win,
 								win->img, 0, 0);
