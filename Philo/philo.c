fb8f684170880f8bff54e6fa04e5f6a50098dca6
Shou Xiang <weeshouxiang@gmail.com>
2025-09-20 22:48:17 +0800

    merge files

---
+++ Philo/philo.c
@@ -0,0 +1,8 @@
+#include "philo.h"
+
+int main(int ac, char **av)
+{
+	pthread_t thread;
+
+	if (pthread_create())
+}
--- experiment/Sudoku_solver/sudoku.c
+++ experiment/Sudoku_solver/sudoku.c
@@ -1,41 +1,6 @@
 #include "sudoku.h"
 
-int main(int ac, char **av)
+int	main(void)
 {
-	t_var 	var;
-	int		iter;
-	int		**arr;
-
-	if (ac == 2)
-	{
-		var.i = 0;
-		arr = (int **)malloc(sizeof(int *) * (ac - 1) * 9);
-		iter = 1;
-		while (av[iter])
-		{
-			var.j = 0;
-			arr[var.i] = (int *)malloc(sizeof(int) * 9);
-			while(var.j < 9)
-			{
-				arr[var.i][var.j] = atoi(av[iter]);
-				printf("Number is %d\n", arr[var.i][var.j]);
-				iter++;
-			var.j++;
-			}
-		var.i++;
-	}
-	var.i = 0;
-	while (var.i < 9)
-	{
-		var.j = 0;
-		while (var.j < 9)
-		{
-			printf("arr %d = %d\n", var.i, arr[var.i][var.j]);
-			var.j++;
-		}
-		var.i++;
-	}
-	fint(arr, ac - 1);
-	return (0);
-	}
+	char	sudoku[9][9] = {"1", "2", "3", "4"}
 }
