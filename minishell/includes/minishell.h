375672cfc0f7364a2de2c571bd340ab24d4d8496
shou xiang <weeshouxiang@gmail.com>
2025-10-19 19:51:33 +0800

    started minishell

---
+++ minishell/includes/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshou-xi <wshou-xi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 19:46:04 by wshou-xi          #+#    #+#             */
+/*   Updated: 2025/10/19 19:49:55 by wshou-xi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <sys/types.h>
+
+
+#endif
