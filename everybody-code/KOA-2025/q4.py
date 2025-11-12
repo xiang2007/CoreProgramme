42fbae487bfbd12ba49eabcf6f38dec8a9560b0f
shou xiang <weeshouxiang@gmail.com>
2025-11-12 22:21:40 +0800

    added files

--- everybody-code/KOA-2025/q4.py
+++ everybody-code/KOA-2025/q4.py
@@ -3,7 +3,7 @@ class node:
 		self.prev = None
 		self.data = data
 		self.next = None
-		self.bottom = Node
+		self.bottom = None
 
 x = [5,3,7,8,9,10,4,5,7,8,8]
 
@@ -12,11 +12,17 @@ curr = None
 
 # add node2 to left side of node1
 def	add_left(node1, value):
+	new_node = node(value)
+	node1.left = new_node
 	node1.prev = node(value)
 
 # add node2 to right side of node1
 def	add_right(node1, value):
 	node1.next = node(value)
+
+# add a new node below node1
+def	add_down(node1, value):
+	node1.bottom = node(value)
 
 for i in range(len(x) - 1):
 	curr = node[i]
---
+++ everybody-code/KOA-2025/test.py
@@ -0,0 +1,9 @@
+one = 1
+x = 10
+y = 20
+int_arr = [1,2,3,4,5,6,7,8,9,10]
+string = ["a,b,c,d,e,f,g"]
+s_split = ['a','b','c','d','e']
+
+listed = list(enumerate(s_split, start = 1))
+print(listed)
