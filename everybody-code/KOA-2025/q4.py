06582976da980405bc5b824a408f006723576daa
shou xiang <weeshouxiang@gmail.com>
2025-11-12 11:03:45 +0800

    added q4.py

---
+++ everybody-code/KOA-2025/q4.py
@@ -0,0 +1,27 @@
+class node:
+	def __init__(self, data):
+		self.prev = None
+		self.data = data
+		self.next = None
+		self.bottom = Node
+
+x = [5,3,7,8,9,10,4,5,7,8,8]
+
+head = node(x[0])
+curr = None
+
+# add node2 to left side of node1
+def	add_left(node1, value):
+	node1.prev = node(value)
+
+# add node2 to right side of node1
+def	add_right(node1, value):
+	node1.next = node(value)
+
+for i in range(len(x) - 1):
+	curr = node[i]
+	if (curr.data > x[i + 1]):
+		add_left(curr, x[i])
+	elif (curr.data < x[i + 1]):
+		add_left(curr, x[i])
+
