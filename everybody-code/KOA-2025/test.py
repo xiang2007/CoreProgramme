b6a2abff2445b5492845bfd9e8c05b48d86c6950
Wee Shou Xiang <weeshouxiang@gmail.com>
2025-11-20 10:17:25 +0800

    splitting the questions into several parts

--- everybody-code/KOA-2025/test.py
+++ everybody-code/KOA-2025/test.py
@@ -1,9 +1,18 @@
-one = 1
-x = 10
-y = 20
-int_arr = [1,2,3,4,5,6,7,8,9,10]
-string = ["a,b,c,d,e,f,g"]
-s_split = ['a','b','c','d','e']
+data = """1:2,4,1,1,8,2,7,9,8,6
+2:7,9,9,3,8,3,8,8,6,8
+3:4,7,6,9,1,8,3,7,2,2
+4:6,4,2,1,7,4,5,5,5,8
+5:2,9,3,8,3,9,5,2,1,4
+6:2,4,9,6,7,4,1,7,6,8
+7:2,3,7,6,2,2,4,1,4,2
+8:5,1,5,6,8,3,1,8,3,9
+9:5,7,7,3,7,2,3,8,6,7
+10:4,1,9,3,8,5,4,3,5,5"""
 
-listed = list(enumerate(s_split, start = 1))
-print(listed)
+lines = data.strip().splitlines()
+arrays = []
+for line in lines:
+	_, values = line.split(':', 1)
+	arrays.append([int(n) for n in values.split(",")])
+
+print(arrays)
