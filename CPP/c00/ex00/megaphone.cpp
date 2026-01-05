904cfc67a3aaea338bbd2b2743eec9ea63c273a8
shou xiang <weeshouxiang@gmail.com>
2026-01-05 22:05:30 +0800

    added cpp and filterv2

---
+++ CPP/c00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+int	main(int ac, char **av)
+{
+	std::string res;
+
+	if (ac == 1)
+		return (std::cout << "* LOUD AND UNREADABLE FEEDBACK NOISE *\n", 0);
+	else {
+		for (int i=1; i<ac; i++)
+		{
+			for (int j=0; j<(int)strlen(av[i]); j++){
+				if (islower(av[i][j]))
+					res += toupper(av[i][j]);
+				else
+					res += av[i][j];
+			}
+		}
+	}
+	std::cout << res + '\n';
+}
