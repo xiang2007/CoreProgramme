2c010e221c2b9fbac3468bc029e65c34f0c766fa
shou xiang <weeshouxiang@gmail.com>
2026-02-04 19:38:17 +0800

    added ex02

--- CPP/c01/ex02/main.cpp
+++ CPP/c01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wshou-xi <wshou-xi@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 18:43:57 by wshou-xi          #+#    #+#             */
-/*   Updated: 2026/02/04 19:01:25 by wshou-xi         ###   ########.fr       */
+/*   Updated: 2026/02/04 19:25:16 by wshou-xi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,14 @@
 int	main()
 {
 	std::string str = "HI THIS IS BRAIN";
-	std::string stringPTR& = str;
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Memory address of string " << str << " is: " << &str << "\n";
+	std::cout << "Memery address of stringPTR is: " << &stringPTR << "\n";
+	std::cout << "Memory address of stringREF is: " << &stringREF << "\n";
+	std::cout << "Value of string is " << str << "\n";
+	std::cout << "Value of stringPTR is: " << stringPTR << "\n";
+	std::cout << "Value of stringREF is: " << stringREF << std::endl;
+	return 0;
 }
