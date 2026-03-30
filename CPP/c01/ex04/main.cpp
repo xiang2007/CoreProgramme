8eed7570a4eb4b1b831dca58d5469f9fe36e858e
Shou Xiang <weeshouxiang@gmail.com>
2026-03-31 00:40:16 +0800

    harl

--- CPP/c01/ex04/main.cpp
+++ CPP/c01/ex04/main.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
 #include <iosfwd>
@@ -12,37 +13,40 @@ using std::fstream;
 using std::ofstream;
 using std::ios;
 
-int	main(int ac, char **av)
-{
-	fstream	file_in;
+int main(int ac, char **av) {
 	if (ac != 4)
-		return (cout << "Invalid arguments, <filename> <s1> <s2>" << endl, 1);
+		return (cout << "invalid arguments\n<filename> <s1> <s2>" << endl, 1);
+	string sub = av[2];
+	string trp = av[3];
+	if (sub.empty() || trp.empty())
+		return (cout << "Empty arguments" << endl, 1);
+	fstream file_in;
 	file_in.open(av[1], ios::in);
 	if (!file_in)
-		return (cout << "Invalid infile" << endl, 1);
-	string sub = av[2];
-	string to_replace = av[3];
-	if (sub.empty() || to_replace.empty()) {
-		return (cout << "Argument is empty" << endl, 1);
-	}
-	string temp_out_name = av[1];
-	string file_out_name = temp_out_name + ".replace";
-	ofstream outfile(file_out_name.c_str());
-	outfile.open(file_out_name.c_str(), ios::out);
-	string buffer;
+		return (cout << "File '" << av[1] << "' not found" << endl, 1);
+	if (!file_in.eof())
+		return (cout << "'" << av[1] << "' is a empty file" << endl, 1);
+	string s;
 	while (file_in.is_open()) {
 		string line;
 		while (getline(file_in, line)) {
-			buffer += line + '\n';
+			s += line + "\n";
 		}
 		file_in.close();
 	}
-	// size_t res = -1;
-	cout << buffer;
-	// while ((res = buffer.find(sub, res + 1) != string::npos)) {
-	// 	buffer.erase(buffer.begin() + res);
-	// 	// buffer.insert(res, to_replace);
-	// 	cout << res << " ";
-	// }
-	// cout << buffer << endl;
+	size_t res = -1;
+	int s_size = trp.size();
+	int	size = sub.size();
+	while ((res = s.find(sub, res + 1)) != string::npos) {
+		s.erase(s.begin() + res, s.begin() + res + size);
+		s.insert(res, trp.c_str());
+		res += s_size - 1;
+	}
+	s.erase(s.size() - 1);
+	string out = av[1];
+	string outres = out + ".replace";
+	ofstream outfile(outres.c_str());
+	outfile << s;
+	outfile.close();
+	return 0;
 }
---
+++ CPP/c01/ex05/Harl.cpp
@@ -0,0 +1,29 @@
+#include "Harl.hpp"
+
+void	Harl::debug(void)
+{
+	cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << endl;
+}
+
+void	Harl::info(void)
+{
+	cout << "I cannot believe adding extra bacon costs more money. You didn't putenough bacon in my burger! If you did, I wouldn't be asking for more!" <<endl;
+}
+
+void	Harl::warning(void)
+{
+	cout << "I think I deserve to have some extra bacon for free. I've been coming foryears, whereas you started working here just last month." << endl;
+}
+
+void	Harl::error(void)
+{
+	cout << "This is unacceptable! I want to speak to the manager now." << endl;
+}
+
+void	Harl::complain(string level)
+{
+	switch (level.empty())
+	{
+		case 
+	}
+}
